c400db8e5e4a919b57ea4f7bd6d77b027c35cfda
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-13 13:58:32 +0200

    Add docstrings

--- src/app/lib/SearchResponse.ts
+++ src/app/lib/SearchResponse.ts
@@ -1,4 +1,16 @@
+/**
+ * Interface for API responses
+ *
+ * typifies the responses in order to keep 
+ * them tight to the app. For examples, the
+ * {@link ./SearchService | SearchService} uses
+ * only event emitter that has the kind of response
+ * this app supports.
+ */
 export interface SearchResponse {
+    /** relevant attribute from Google API Response */
     results?;
+
+    /** relevant attribute from DDG API Response */
     RelatedTopics?;
 }
