0ca1519de50cb5abb77f49f78971313a92eaaa78
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-13 14:46:04 +0200

    Add HttpClientModule because of dependency injection

--- src/app/services/google.service.spec.ts
+++ src/app/services/google.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GoogleService } from './google.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 describe('GoogleService', () => {
   let service: GoogleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientModule] });
     service = TestBed.inject(GoogleService);
   });
 
