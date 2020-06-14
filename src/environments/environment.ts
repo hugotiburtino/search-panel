487e0a8293ead8ba662ed91c2d4a98599b7bbc61
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-14 14:45:09 +0200

    Put var for the name of app and add instructino to prod
    And delete commentaries. The name of app is required when making
    calls to DDG API.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// Do not forget to change environment.prod.ts if you plan to use it in production
 
 export const environment = {
   production: false,
   RAPIDAPI_HOST: 'google-search3.p.rapidapi.com',
-  RAPIDAPI_KEY: '' // put your Rapid Key here
+  RAPIDAPI_KEY: '' , // // put your Rapid Key here
+  APP_NAME: 'search-panel' // Put the name of your app here
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
